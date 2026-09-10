b3395110ca4df5d1990a488c90b02c45db022dbb
Hélio Andrade <helioaraujoandrade@outlook.com>
2026-09-09 22:29:45 -0300

    fix: remove ExcludeFromCodeCoverage da interface IPaymentPort (invalido)

--- src/UcpAgent.SharedKernel/Ports/IPaymentPort.cs
+++ src/UcpAgent.SharedKernel/Ports/IPaymentPort.cs
@@ -2,7 +2,6 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace UcpAgent.SharedKernel.Ports;
 
-[ExcludeFromCodeCoverage]
 public interface IPaymentPort
 {
     Task<PaymentResultDto> ProcessAsync(
