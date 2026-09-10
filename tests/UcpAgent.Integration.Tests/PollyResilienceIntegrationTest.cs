013ebfc6cdab36e65ec3d07a144fecff53dd5d06
Hélio Andrade <helioaraujoandrade@outlook.com>
2026-09-10 19:16:39 -0300

    fix(test): adiciona using Microsoft.AspNetCore.TestHost para ConfigureTestServices

--- tests/UcpAgent.Integration.Tests/PollyResilienceIntegrationTest.cs
+++ tests/UcpAgent.Integration.Tests/PollyResilienceIntegrationTest.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Text;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
